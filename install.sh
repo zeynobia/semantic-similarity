9468d7ee687d2ed3a7c5655a3dba80cf08e3cea5
Zeynep Yilmaz <74449808+zeynobia@users.noreply.github.com>
2023-11-06 10:28:34 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+pip3 install gensim==3.8.0 #3.8.0
+pip3 install memory_profiler
+pip3 install numpy
+pip3 install uuid
+pip3 install numba
+pip3 install nltk
+pip3 install simplejson
+pip3 install flask
+
+pip3 install yake # phrase extraction algorithm
+pip3 install summa  # phrase extraction algorithm
+pip3 install rake-nltk  # phrase extraction algorithm
+# pip3 install keyBERT # optional  (phrase extraction algorithm)
+pip3 install sumy
+pip3 install datasketch
+pip3 install scikit-learn #==0.21.2
+pip3 install enstop
+python3 -m nltk.downloader stopwords
